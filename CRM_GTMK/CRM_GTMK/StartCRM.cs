f06931870cb5200dfbbb8b806b5fb39ef3f59bf3
Victor Kuryshev <sales@gtmk.ru>
2017-11-13 12:40:44 +0300

    First Commit

---
+++ CRM_GTMK/CRM_GTMK/StartCRM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_GTMK
+{
+    class StartCRM
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
