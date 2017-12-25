d86584879f99347d7be7d77b2b67e787f0492afa
Vladimir Beliakov <vladimirred456@gmail.com>
2017-12-26 00:53:47 +0300

    В форме добавления нового сотрудника сделал все элементы согласно ТЗ из issue Root 2, за исключение панели телефонов (на данный момент телефоны просто добавляются по нажатию на кнопку "Добавить телефон", а не через отдельную форму). Сделал классы для элементов (телефоны и комментарии), которые добавляются при нажатии на соответствующие кнопки. Данная версия содержит только графическую часть ТЗ из issue Root 2. Вводимые данные пока не сохраняются.

---
+++ CRM_GTMK/CRM_GTMK/Visual/AddCompanyPanels/OfficesPanel/ContactPersonPanel/CommentsContactPersonFlowLayoutPanel/MyCommentsContactPersonFlowLayoutPanel.cs
@@ -0,0 +1,28 @@
+﻿using CRM_GTMK.Visual.AddCompanyPanels.OfficesPanel.ContactPersonPanel.CommentsContactPersonFlowLayoutPanel.CommentPanel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRM_GTMK.Visual.AddCompanyPanels.OfficesPanel.ContactPersonPanel.CommentsContactPersonFlowLayoutPanel
+{
+    public class MyCommentsContactPersonFlowLayoutPanel : FlowLayoutPanel
+    {
+        public MyCommentsContactPersonFlowLayoutPanel(AddNewContactPersonForm form)
+        {
+            FlowLayoutPanel contactPersonFlowLayoutPanel = form.GetCommentsContactPersonFlowLayoutPanel();
+
+            WrapContents = contactPersonFlowLayoutPanel.WrapContents;
+            Anchor = contactPersonFlowLayoutPanel.Anchor;
+            AutoScroll = contactPersonFlowLayoutPanel.AutoScroll;
+            AutoSize = contactPersonFlowLayoutPanel.AutoSize;
+            FlowDirection = contactPersonFlowLayoutPanel.FlowDirection;
+            Location = contactPersonFlowLayoutPanel.Location;
+            Name = contactPersonFlowLayoutPanel.Name;
+            Size = contactPersonFlowLayoutPanel.Size;
+            TabIndex = contactPersonFlowLayoutPanel.TabIndex;
+        }
+    }
+}
---
+++ CRM_GTMK/CRM_GTMK/Visual/AddCompanyPanels/OfficesPanel/ContactPersonPanel/CommentsContactPersonFlowLayoutPanel/OneCommentPanel/CommentPanelElements/MyCommentRichTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRM_GTMK.Visual.AddCompanyPanels.OfficesPanel.ContactPersonPanel.CommentsContactPersonFlowLayoutPanel.OneCommentPanel.CommentPanelElements
+{
+    public class MyCommentRichTextBox : RichTextBox
+    {
+        public MyCommentRichTextBox(AddNewContactPersonForm form)
+        {
+            RichTextBox commentRichTextBox = form.GetCommentRichTextBox();
+
+            Location = commentRichTextBox.Location;
+            Name = commentRichTextBox.Name;
+            Size = commentRichTextBox.Size;
+            TabIndex = commentRichTextBox.TabIndex;
+            Text = commentRichTextBox.Text;
+        }
+    }
+}
